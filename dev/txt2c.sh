d494ce253aa2fd983c396c04312e9895c32c0417
Sebastian Riedel <git@basxto.de>
2020-03-31 03:44:18 +0200

    Allow comments in string file

--- dev/txt2c.sh
+++ dev/txt2c.sh
@@ -1,11 +1,12 @@
 #!/bin/bash
 IFS='
 '
-value=(`sed 's/^.*$/"&";/g' $1 | tr '[:lower:]' '[:upper:]'`)
-name=(`sed 's/[^a-z0-9A-Z]//g' $1 | tr '[:upper:]' '[:lower:]'`)
+value=(`grep -o '^[^#]*' $1 | tr '[:lower:]' '[:upper:]'`)
+name=(`grep -o '^[^#]*' $1 | sed 's/[^a-z0-9A-Z]//g' | tr '[:upper:]' '[:lower:]'`)
 
 echo "// Generated with text2c.sh" > $2
+echo "#define strlen(x) (sizeof(x) - 1)" >> $2
 
 for i in `seq 0 $((${#name[@]}-1))`; do
-    echo const unsigned char text_${name[i]:0:8}[] = ${value[i]} >> $2
+    echo const unsigned char text_${name[i]:0:8}[] = \"${value[i]}\"\; >> $2
 done

cb41398c33f9f86fc71dbe6026aeead785bb6901
Sebastian Riedel <git@basxto.de>
2020-04-08 12:23:33 +0200

    Improve map

---
+++ level/level.h
@@ -0,0 +1,12 @@
+
+#ifndef LEVEL_H
+#define LEVEL_H
+
+#include <gb/gb.h>
+
+typedef struct {
+    const UINT8 *background;
+    const UINT8 *collision;
+} Level;
+
+#endif
---
+++ level/sand_bottle.tsx
@@ -0,0 +1,4 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.2" tiledversion="1.3.3" name="sand_bottle" tilewidth="16" tileheight="16" tilecount="1" columns="1">
+ <image source="../pix/sand_bottle.png" trans="ff00ff" width="16" height="16"/>
+</tileset>

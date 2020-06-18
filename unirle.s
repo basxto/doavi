3388c4b3eb4f417ae4904562473cde7d6e62bdbf
Sebastian Riedel <git@basxto.de>
2020-06-19 00:34:39 +0200

    [IRLE] Change format

--- unirle.s
+++ unirle.s
@@ -2,47 +2,40 @@
 ; Incremental RLE decompression for Game Boy
 ; This is useful for tilemaps with long incremental sequences
 ;  and values between 0 and 127
-; It compresses to 0.8%-100% of the original size
-; The worst case can be avoided by changing the tile order
-;  in the image loaded to vram
+; It compresses to 0.8%-200% of the original size
 ;
-; Code:
-; 0XXX XXXX are literals
-; 1XXX XXXX increments <0XXX XXXX> by <next byte> times
-; -> 0x00 is 256 times
-;
-; Example:
-; DE: 0x80 0x03 0x01
-; HL: 0x00 0x01 0x02 0x01
+; by basxto and ISSOtm
 
-	.area	_CODE
-	; unpack DE to HL for B decompressed bytes
-	; A is incremental counter
-	; C is the byte
-.irle_unpack_block::
-	ld	a, #0x01
-.irle_unpack_loop:
-	dec a
-	jr nz, .irle_unpack_noload
-	; A is 0
+; Format:
+; 0000 0000 terminate
+; CCCC CCC0 AAAA AAAC write A++ C times
+; AAAA AAA1 write A
+; @param de Data stream being Encoded as above
+; @param hl Destination buffer
+.irle_unpack::
+	ld c, #0
+.irle_cmd:
 	ld a, (de)
 	inc de
-	ld c, a
-	; highest bit decides mode
-	bit 7, a
-	jr z, .irle_unpack_literal
-	; remove command bit
-	res 7, c
-	; load amount
-	ld a, (de)
-	inc de
-	jr .irle_unpack_noload
-.irle_unpack_literal:
-	; one run
-	ld	a, #0x01
-.irle_unpack_noload:
-	ld (hl+), c
 	inc c
-	dec b
-	jr nz,.irle_unpack_loop
-	ret
+	srl a
+	jr C, .irle_write
+	; C is 0
+	ret Z
+	; a was upper 7 bits of counter
+	ld c, a
+	ld a, (de)
+	inc de
+	; move last bit of a to c
+	rra
+	rl c
+.irle_write:
+	ld (hl+), a
+	inc a
+	dec c
+	jr NZ, .irle_write
+	; c is 0
+	jr .irle_cmd
+
+
+

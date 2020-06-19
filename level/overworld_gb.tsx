3b26cf8d1c218eb7ae74c8294bad88291871cb2a
Sebastian Riedel <git@basxto.de>
2020-06-19 06:38:24 +0200

    Add better paths

--- level/overworld_gb.tsx
+++ level/overworld_gb.tsx
@@ -16,4 +16,34 @@
  <tile id="11" terrain="0,1,1,0"/>
  <tile id="12" terrain="0,1,0,0"/>
  <tile id="13" terrain="1,0,0,0"/>
+ <wangsets>
+  <wangset name="path" tile="23">
+   <wangedgecolor name="grass" color="#ff0000" tile="-1" probability="1"/>
+   <wangedgecolor name="path" color="#00ff00" tile="-1" probability="1"/>
+   <wangtile tileid="0" wangid="0x1010101"/>
+   <wangtile tileid="1" wangid="0x1010101"/>
+   <wangtile tileid="2" wangid="0x1010101"/>
+   <wangtile tileid="3" wangid="0x1010101"/>
+   <wangtile tileid="4" wangid="0x1010101"/>
+   <wangtile tileid="20" wangid="0x1010101"/>
+   <wangtile tileid="21" wangid="0x1020102"/>
+   <wangtile tileid="22" wangid="0x2010201"/>
+   <wangtile tileid="23" wangid="0x2020202"/>
+  </wangset>
+  <wangset name="tree" tile="12">
+   <wangcornercolor name="grass" color="#ff0000" tile="-1" probability="1"/>
+   <wangcornercolor name="tree" color="#00ff00" tile="-1" probability="1"/>
+   <wangtile tileid="0" wangid="0x10101010"/>
+   <wangtile tileid="1" wangid="0x10101010"/>
+   <wangtile tileid="2" wangid="0x10101010"/>
+   <wangtile tileid="3" wangid="0x10101010"/>
+   <wangtile tileid="4" wangid="0x10101010"/>
+   <wangtile tileid="8" wangid="0x10202020"/>
+   <wangtile tileid="9" wangid="0x20202010"/>
+   <wangtile tileid="10" wangid="0x20102010"/>
+   <wangtile tileid="11" wangid="0x10201020"/>
+   <wangtile tileid="12" wangid="0x20102020"/>
+   <wangtile tileid="13" wangid="0x20201020"/>
+  </wangset>
+ </wangsets>
 </tileset>
---
+++ level/path.tsx
@@ -0,0 +1,24 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.2" tiledversion="1.3.3" name="path" tilewidth="16" tileheight="16" tilecount="16" columns="8">
+ <image source="../pix/overworld_path_gbc.png" trans="ff00ff" width="128" height="32"/>
+ <wangsets>
+  <wangset name="path" tile="6">
+   <wangedgecolor name="grass" color="#ff0000" tile="-1" probability="1"/>
+   <wangedgecolor name="path" color="#00ff00" tile="-1" probability="1"/>
+   <wangtile tileid="0" wangid="0x1020201"/>
+   <wangtile tileid="1" wangid="0x2020101"/>
+   <wangtile tileid="2" wangid="0x1010201"/>
+   <wangtile tileid="3" wangid="0x1020101"/>
+   <wangtile tileid="4" wangid="0x1020202"/>
+   <wangtile tileid="5" wangid="0x2020201"/>
+   <wangtile tileid="6" wangid="0x2020202"/>
+   <wangtile tileid="7" wangid="0x1010101"/>
+   <wangtile tileid="8" wangid="0x1010202"/>
+   <wangtile tileid="9" wangid="0x2010102"/>
+   <wangtile tileid="10" wangid="0x2010101"/>
+   <wangtile tileid="11" wangid="0x1010102"/>
+   <wangtile tileid="12" wangid="0x2020102"/>
+   <wangtile tileid="13" wangid="0x2010202"/>
+  </wangset>
+ </wangsets>
+</tileset>

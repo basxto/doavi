3c10f3d6c430f0e9d2dcb11580ecf7e13b55c994
Sebastian Riedel <git@basxto.de>
2020-11-26 08:27:54 +0100

    Fix assembly syntax

--- unlz3.s
+++ unlz3.s
@@ -50,43 +50,43 @@ _lz3_unpack_block::
 
 ._lz3_unpack_block_header:
 	; read 1byte header
-	ld a, [de] ; CCCL LLLL
+	ld a, (de) ; CCCL LLLL
 	inc de
-	ld [hl], a ; store it
+	ld (hl), a ; store it
 	ld b, a
 	and a, #0x1F
 	ld c, a
 ._lz3_unpack_block_command:
 	inc c ; 0 is always once
 
-	bit 8, b
+	bit 7, b
 	jr NZ, ._lz3_unpack_block_1XX
 ._lz3_unpack_block_0XX:
-	bit 7, b
+	bit 6, b
 	jr NZ, ._lz3_unpack_block_01X
 ._lz3_unpack_block_00X:
-	bit 6, b
+	bit 5, b
 	jr NZ, ._lz3_unpack_block_001
 ._lz3_unpack_block_000: ; direct copy
-	ld a, [de]
-	ld [hl+], a
+	ld a, (de)
+	ld (hl+), a
 	inc de
 	dec c
 	jr NZ, ._lz3_unpack_block_000
 	jr ._lz3_unpack_block_header
 ._lz3_unpack_block_001: ; byte fill
-	ld a, [de]
+	ld a, (de)
 	inc de
 	ld b, a
 	jr ._lz3_unpack_block_fill
 ._lz3_unpack_block_01X:
-	bit 6, b
+	bit 5, b
 	jr NZ, ._lz3_unpack_block_011
 ._lz3_unpack_block_010: ; word fill
-	ld a, [de]
+	ld a, (de)
 	inc de
 	ld b, a
-	ld a, [de]
+	ld a, (de)
 	inc de
 	jr ._lz3_unpack_block_fill
 ._lz3_unpack_block_011: ; zero fill
@@ -94,22 +94,22 @@ _lz3_unpack_block::
 	ld b, a
 	jr ._lz3_unpack_block_fill
 ._lz3_unpack_block_1XX:
-	bit 7, b
+	bit 6, b
 	jr NZ, ._lz3_unpack_block_11X
 ._lz3_unpack_block_10X:
-	bit 6, b
+	bit 5, b
 	jr NZ, ._lz3_unpack_block_101
 ._lz3_unpack_block_100: ; repeat
 	jr ._lz3_unpack_block_start
 ._lz3_unpack_block_101: ; bit-reverse repeat
 	jr ._lz3_unpack_block_start
 ._lz3_unpack_block_11X:
-	bit 6, b
+	bit 5, b
 	jr NZ, ._lz3_unpack_block_111
 ._lz3_unpack_block_110: ; backwards repeat
 	jr ._lz3_unpack_block_start
 ._lz3_unpack_block_111: ; long length
-	ld a, [de] ; LLLL LLLL
+	ld a, (de) ; LLLL LLLL
 	inc de
 	ld c, a
 	; b<<3
@@ -124,11 +124,11 @@ _lz3_unpack_block::
 ; b: first byte  to write (alternate)
 ; c: amount (1 is once)
 ._lz3_unpack_block_fill:
-	ld [hl], b
+	ld (hl), b
 	inc hl
 	dec c
 	jr Z, ._lz3_unpack_block_header
-	ld [hl+], a
+	ld (hl+), a
 	dec c
 	jr NZ, ._lz3_unpack_block_fill
 	jr ._lz3_unpack_block_header
@@ -139,9 +139,9 @@ _lz3_unpack_block::
 ; pushes de
 ; sets de to address
 ._lz3_unpack_block_start:
-	ld a, [de]
+	ld a, (de)
 	inc de
-	bit 8, a
+	bit 7, a
 	jr NZ, ._lz3_unpack_block_start_extended
 	push de
 	push bc
@@ -154,10 +154,10 @@ _lz3_unpack_block::
 	push de
 	push bc
 	; first bit ignored
-	res 8, a
+	res 7, a
 	ld b, a ; upper byte
 	dec de
-	ld a, [de]
+	ld a, (de)
 	inc de
 	ld c, a ; lower byte
 	; set de to dest buffer start
@@ -180,27 +180,27 @@ _lz3_unpack_block::
 ; b: command
 ; c: amount (1 is once)
 ._lz3_unpack_block_repeat:
-	ld a, [de]
-	bit 6, b
+	ld a, (de)
+	bit 5, b
 	jr Z, ._lz3_unpack_block_repeat_skip_rev
-	ld [hl], b
+	ld (hl), b
 	.rept 8
 	rla
 	rr b
 	.endm
 	ld a, b
-	ld b, [hl]
+	ld b, (hl)
 	inc de 
 	jr ._lz3_unpack_block_repeat_skip_dec
 ._lz3_unpack_block_repeat_skip_rev:
-	bit 7, b
+	bit 6, b
 	inc de 
 	jr Z, ._lz3_unpack_block_repeat_skip_dec
 	dec de
 	dec de
 ._lz3_unpack_block_repeat_skip_dec:
-	ld [hl+], a
+	ld (hl+), a
 	dec c
 	jr NZ, ._lz3_unpack_block_repeat
 	pop de
-	jr ._lz3_unpack_block_header
+	jp ._lz3_unpack_block_header
